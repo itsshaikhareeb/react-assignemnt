0568faa305c09ee844cc25e43306a1814a72ae71
Shaikh Areeb <shaikhareeb321@gmail.com>
2026-07-01 14:57:44 +0530

    add fucntionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
-  // const [count, setCount] = useState(0)
-  let count = 0;
-  function inc(){
-    count++;
-    console.log(count);
+
+  const [count, setCount] = useState(1);
+  const [photos, setPhotos] = useState([]);
+   useEffect(()=>{
+        fetch(` https://api.thecatapi.com/v1/images/search`)
+      .then((res) => res.json())
+      .then((data)=>{
+        console.log(data);
+        setPhotos(data);
+      });
+    },[count]);
+    
+    function image(){
+      
+        setCount(count +1)
   }
-  
-  function dec(){
-    count--;
-    console.log(count);
-  }
+   
 
   return (
     <>
-    <h1>Counter App</h1>
+    <h1>Cat Image App</h1>
     <div>
+      <div className='buttons' >
+      <button onClick={image} > Generate Cat image</button>
+      </div>
+      
+        <div className='images' >
 
-    <button onClick={inc} >Increase</button>
-    <p>count: {count}</p>
-    <button onClick={dec} >Decrease</button>
+        {photos.map((photo)=>(
+          <img key={photo.id} className='single'
+          src={photo.url}
+          />
+        ))}
+        </div>
     </div>
+    
     </>
   )
+
 }
 
 export default App
